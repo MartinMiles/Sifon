4b1d5e93556deea5c28b7c378422f7474d3b6b58
Martin Miles <miles@martin.by>
2019-12-15 23:32:50 +0000

    comment added

--- Sifon.Shared/ScriptGenerators/ServiceScriptGenerator.cs
+++ Sifon.Shared/ScriptGenerators/ServiceScriptGenerator.cs
@@ -11,6 +11,7 @@
 
         public string Start(string serviceName, int percents)
         {
+            // TODO: when getting service also ask to get those that are active
             return $@"
                 $service = Get-Service '{_prefix}*-{serviceName}' -ErrorAction SilentlyContinue
                 if($service -And $service.Status -ne 'Running')
