6b23998b129b967693570e645d017d7a7e51fb6d
Martin Miles <miles@martin.by>
2020-09-09 17:38:22 +0100

    Fixed annoying null-reference on remote Commerce PowerShell when retrieving databases, due to changes in XC10

--- Core/Get-CommerceDatabases.ps1
+++ Core/Get-CommerceDatabases.ps1
@@ -8,20 +8,19 @@ $config2 = "$Webroot\wwwroot\bootstrap\Global.json"
 If(Test-Path $config1){
 
     $json = (Get-Content $config1 -Raw) | ConvertFrom-Json
-    $json.Policies.'$values'.Database
+    if($json.Policies.'$values'.Database)
+    {
+        $json.Policies.'$values'.Database
+    }
 }
 
 If(Test-Path $config2){
 
     $json = (Get-Content $config2 -Raw)  | ConvertFrom-Json
-    #$json.Policies.'$values'[1].Database
-    If(Test-Path $config2){
-
-	$json = (Get-Content $config2 -Raw)  | ConvertFrom-Json
-	#    $json.Policies.'$values'[0].Database
-    
-	ForEach ($value in $json.Policies.'$values') {
-		$value.Database
-	}
-    }
+    	ForEach ($value in $json.Policies.'$values') {
+		if($value.Database)
+        {
+            $value.Database
+        }
+	}    
 }
