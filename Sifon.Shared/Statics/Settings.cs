d0e079502fdd8e6fd18952b0515c7c1c099e6412
Martin <miles@martin.by>
2020-09-18 14:46:59 +0100

    Updated versioning info

--- Sifon.Shared/Statics/Settings.cs
+++ Sifon.Shared/Statics/Settings.cs
@@ -7,7 +7,7 @@ namespace Sifon.Shared.Statics
 {
     public static class Settings
     {
-        public const string ProductVersion = "Sifon v0.9.5 (BETA)";
+        public const string ProductVersion = "Sifon v0.9.6 (BETA)";
         public const string BackupInfoFile = "BackupInfo.xml";
         public const string RemoteDirectory = "Sifon";
 
