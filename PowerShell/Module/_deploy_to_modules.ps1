8551ba06a928f3edce65e59d9b56f87a3a0b0edb
Martin <miles@martin.by>
2020-09-25 13:15:11 +0100

    Deploy_to_modules: now is aware of current directory

--- PowerShell/Module/_deploy_to_modules.ps1
+++ PowerShell/Module/_deploy_to_modules.ps1
@@ -1,8 +1,8 @@
 $ModulesDirectory = "c:\Program Files\WindowsPowerShell\Modules\Sifon"
-
+$psd1 = (split-path -parent $MyInvocation.MyCommand.Definition) + "\Sifon.psd1"
 
 $manifest = @{
-	Path 			    = "Sifon.psd1"
+	Path 			    = $psd1
 	Author 			    = 'Martin Miles'
 	CompanyName 		= 'Perfecta Ltd'
 	Copyright 		    = '(c)2020 Martin Miles. All rights reserved.'
@@ -10,7 +10,7 @@ $manifest = @{
 	GUID 			    = 'e382b374-6ffb-4ae7-9b40-0f99518640ad'
 	ModuleVersion 		= '0.9.6'
 	PowerShellVersion 	= '5.1'
-	FunctionsToExport 	= 'Get-InstanceUrl', 'Install-SitecorePackage','Get-ConnectionString','Download-Resource','Copy-FileToRemote','Get-SiteFolder'
+	FunctionsToExport 	= 'Get-InstanceUrl', 'Install-SitecorePackage'
 	ReleaseNotes 		= 'Accompanies Sifon. You may obtain your copy from https://Sifon.UK'
 	RootModule 		    = 'Sifon.psm1'
 }
