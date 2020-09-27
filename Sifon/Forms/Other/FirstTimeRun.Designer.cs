61a490ae97a573782830d2abf2c50d634f5fbac9
Martin <miles@martin.by>
2020-09-28 00:22:06 +0100

    First time run message adjusted for better

--- Sifon/Forms/Other/FirstTimeRun.Designer.cs
+++ Sifon/Forms/Other/FirstTimeRun.Designer.cs
@@ -36,6 +36,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -75,9 +77,9 @@
             this.label1.ForeColor = System.Drawing.Color.Red;
             this.label1.Location = new System.Drawing.Point(16, 103);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(277, 13);
+            this.label1.Size = new System.Drawing.Size(269, 13);
             this.label1.TabIndex = 15;
-            this.label1.Text = "Once done, you\'ll be able to process with the rest of them";
+            this.label1.Text = "Once done, you\'ll be able to process with the rest of the";
             // 
             // label2
             // 
@@ -85,9 +87,9 @@
             this.label2.ForeColor = System.Drawing.Color.Red;
             this.label2.Location = new System.Drawing.Point(16, 119);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(276, 13);
+            this.label2.Size = new System.Drawing.Size(281, 13);
             this.label2.TabIndex = 16;
-            this.label2.Text = "Most Sifon features will not be available until at least one ";
+            this.label2.Text = "settings - as the bare minimun requres choosing a website ";
             // 
             // label3
             // 
@@ -95,12 +97,14 @@
             this.label3.ForeColor = System.Drawing.Color.Red;
             this.label3.Location = new System.Drawing.Point(16, 135);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(144, 13);
+            this.label3.Size = new System.Drawing.Size(257, 13);
             this.label3.TabIndex = 17;
-            this.label3.Text = "profile is set up and selected.";
+            this.label3.Text = "folder, setting and testing SQL and Solr connectivity. ";
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.labelWarn1);
             this.groupBox1.Controls.Add(this.label3);
@@ -110,7 +114,7 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(314, 200);
+            this.groupBox1.Size = new System.Drawing.Size(314, 254);
             this.groupBox1.TabIndex = 18;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "WARNING!";
@@ -118,18 +122,38 @@
             // button1
             // 
             this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button1.Location = new System.Drawing.Point(178, 160);
+            this.button1.Location = new System.Drawing.Point(183, 214);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(114, 23);
             this.button1.TabIndex = 18;
             this.button1.Text = "I understand that";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(16, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(281, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Most Sifon features will not become available until at least ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(16, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(208, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "one profile is set up correctly and selected.";
+            // 
             // FirstTimeRun
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(344, 227);
+            this.ClientSize = new System.Drawing.Size(344, 286);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -152,5 +176,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
     }
 }
