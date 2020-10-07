5d37b8fff556ee86e3c4353fe027cc37c7f145c4
Martin Miles <miles@martin.by>
2020-10-07 06:09:58 +0100

    Added new Sifon.Shared project for shared controls library

--- Sifon.Shared/Properties/AssemblyInfo.cs
+++ Sifon.Shared/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Sifon.Code")]
+[assembly: AssemblyTitle("Sifon.Shared")]
 [assembly: AssemblyDescription("PowerShell host for Sitecore that allows running plugins in a context of switchable profiles")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Martin Miles")]
-[assembly: AssemblyProduct("Sifon.Code")]
+[assembly: AssemblyProduct("Sifon.Shared")]
 [assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("1b57ad6c-0e4a-4062-987a-2df59f682cd8")]
+[assembly: Guid("47596ab0-a414-495d-a92c-2db7238a7135")]
 [assembly: AssemblyVersion("0.99")]
 [assembly: AssemblyFileVersion("0.99")]
