ba106c9f9c5130f315ba49bc5329022e1a22163a
Martin Miles <miles@martin.by>
2020-11-16 00:36:59 +0000

    Verify-PortalCredentials changed to use Show-Message

--- PowerShell/Module/Verify-PortalCredentials.ps1
+++ PowerShell/Module/Verify-PortalCredentials.ps1
@@ -1,9 +1,8 @@
 Function Verify-PortalCredentials($PortalCredentials){
 
     if($null -eq $PortalCredentials){
-        Write-Output "-----------------------------------"
-        Write-Error  "Sitecore Portal Credentials missing"
-        Write-Output "-----------------------------------"
+
+        Show-Message -Fore Red -Back White -Text "Sitecore Portal Credentials missing"
         Write-Output "In order to be able downloading the resorces from Sitecore Developers Portal, please enter your Sitecore credintials first."
         Write-Output "You can do that from 'Sitecore Portal Credentials' under Sifon 'Settings' menu."
         Write-Progress -Activity "Downloading resources from Sitecore Developers Portal" -CurrentOperation "Operation complete" -PercentComplete 100
