7a05ff780fb05d08a97ab73fce9f58783542e31c
Martin Miles <miles@martin.by>
2020-12-03 14:25:20 +0000

    Added nuspecs for shared DLLs. Versioning for them has been changes into a three-digits format: v1.01 becomes 1.0.1

--- Sifon.Shared/Properties/AssemblyInfo.cs
+++ Sifon.Shared/Properties/AssemblyInfo.cs
@@ -11,5 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("1b57ad6c-0e4a-4062-987a-2df59f682cd8")]
-[assembly: AssemblyVersion("1.01")]
-[assembly: AssemblyFileVersion("1.01")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+
