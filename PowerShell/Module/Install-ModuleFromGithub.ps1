f6e6acd9d473c4c250752c77447ce7f690d50c21
Martin Miles <miles@martin.by>
2020-12-06 00:59:41 +0000

    Changed Install-ModuleFromGithub funstion of Sifon module

--- PowerShell/Module/Install-ModuleFromGithub.ps1
+++ PowerShell/Module/Install-ModuleFromGithub.ps1
@@ -1,17 +1,15 @@
 function Install-ModuleFromGithub
 (
-	[string]$ModuleName, 
-	[string]$ModuleSubfolder = $ModuleName
+	[string]$UriBase, 
+	[string]$ModuleName
 )
 {
-	$moduleFolder = "c:\Program Files\WindowsPowerShell\Modules\$ModuleSubfolder\"
+	$moduleFolder = "c:\Program Files\WindowsPowerShell\Modules\$ModuleName\"
 	Remove-Item $moduleFolder -Recurse -ErrorAction Ignore
 	New-Item -ItemType Directory -Force -Path $moduleFolder
 
-	$UriBase = "https://raw.githubusercontent.com/Sitecore/Sitecore.HabitatHome.Utilities/master/Shared/assets/modules/$ModuleSubfolder/"
-	$psd = "$ModuleName.psd1"
 	$psm = "$ModuleName.psm1"
-
-	Invoke-RestMethod ($UriBase + $psm) -OutFile ($moduleFolder + $psm)
-	Invoke-RestMethod ($UriBase + $psd) -OutFile ($moduleFolder + $psd)
+	Invoke-RestMethod ($UriBase) -OutFile ($moduleFolder + $psm)
 }
+
+# Install-ModuleFromGithub -ModuleName 'SharedInstallationUtilities' -UriBase "https://raw.githubusercontent.com/Sitecore/Sitecore.HabitatHome.Utilities/master/Shared/assets/modules/SharedInstallationUtilities/SharedInstallationUtilities.psm1"
