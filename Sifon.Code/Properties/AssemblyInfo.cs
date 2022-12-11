802584dafc7edca945a48b8f006e872827dfe3fe
Martin Miles <miles@martin.by>
2022-12-10 17:03:29 -0800

    Version updated to 1.3.3

--- Sifon.Code/Properties/AssemblyInfo.cs
+++ Sifon.Code/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("1b57ad6c-0e4a-4062-987a-2df59f682cd8")]
-[assembly: AssemblyVersion("1.3.2")]
-[assembly: AssemblyFileVersion("1.3.2")]
+[assembly: AssemblyVersion("1.3.3")]
+[assembly: AssemblyFileVersion("1.3.3")]
