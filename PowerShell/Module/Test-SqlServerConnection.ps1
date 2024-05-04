cefa837d8778bdbdb6b754263590fcca87b360cd
WIN11\Martin <miles@martin.by>
2024-05-05 00:41:57 +0100

    Fixed SQL server test connection function by using 'TrustServerCertificate' flag

--- PowerShell/Module/Test-SqlServerConnection.ps1
+++ PowerShell/Module/Test-SqlServerConnection.ps1
@@ -9,10 +9,10 @@ function Test-SqlServerConnection
 	Write-Output "Connecting remote SQL Server $ServerInstance"
 	
 	if($Username -eq $null -or $Password -eq $null){
-		Invoke-Sqlcmd -Hostname $ServerInstance -Credential $SqlCredentials -Query "SELECT GETDATE() AS TimeOfQuery"
+		Invoke-Sqlcmd -Hostname $ServerInstance -Credential $SqlCredentials -Query "SELECT GETDATE() AS TimeOfQuery" -TrustServerCertificate
 	}
 	else
 	{
-		Invoke-Sqlcmd -Query "SELECT GETDATE() AS TimeOfQuery" -ServerInstance "$ServerInstance" -Username "$Username" -Password "$Password"
+		Invoke-Sqlcmd -Query "SELECT GETDATE() AS TimeOfQuery" -ServerInstance "$ServerInstance" -Username "$Username" -Password "$Password" -TrustServerCertificate
 	}	
 }
