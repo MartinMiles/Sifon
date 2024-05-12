44ee754230ced7c565efa0ffffea5a3497d5a231
WIN11\Martin <miles@martin.by>
2024-05-12 12:54:36 +0100

    Added initial version of PowerShell function to install SQL server depending on Edition and Version, respecting parameters passed from Sifon UI

--- PowerShell/Module/Install-SQLServer.ps1
+++ PowerShell/Module/Install-SQLServer.ps1
@@ -6,13 +6,59 @@ function Install-SQLServer
 	[string]$Password = "SA_PASSWORD"
 )
 {
-	#if instance starts with .\ - trim it
+	# Check if $Instance starts with ".\" and remove it
+	if ($Instance.StartsWith(".\")) {
+		$Instance = $Instance -replace '^\.\', ''
+	}
+		
+	# Define the version and package mappings
+	$VersionMappings = @{
+		"Express" = @{
+			"Latest" = "PARAM NOT USED"
+			"2019"   = "2019.20200409"
+			"2017"   = "2017.20190916"
+			"2016"   = "13.0.1601.5"
+		}
+		"Developer" = @{
+			"Latest" = "sql-server-2022"
+			"2022"   = "sql-server-2022"
+			"2019"   = "sql-server-2019"
+			"2017"   = "sql-server-2017"
+			"2016"   = "sql-server-2016-developer-edition"
+		}
+	}
 
-	choco install sql-server-express -o -ia "'/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=$Instance /INSTANCENAME=SQLSERVER /UPDATEENABLED=FALSE /SECURITYMODE=SQL /SAPWD=$Password'" -f -y
-	Invoke-Sqlcmd -Query "ALTER LOGIN sa ENABLE" -ServerInstance ".\$Instance"
-	Invoke-Sqlcmd -Query "ALTER LOGIN sa WITH PASSWORD='$Password'" -ServerInstance ".\$Instance"
+	# Determine the appropriate package and version or command construction
+	$VersionParam = ""
+	if ($Edition -eq "Express" -and $Version -ne "Latest") {
+		$VersionParam = "--version=" + $VersionMappings[$Edition][$Version]
+	}
+	
+	$Package = if ($Edition -eq "Developer") { $VersionMappings[$Edition][$Version] } else { "sql-server-express" }	
 
-	Invoke-Sqlcmd -Query "SELECT GETDATE() AS TimeOfQuery" -ServerInstance ".\$Instance" -Username "sa" -Password "$Password"
+	# Base command for Chocolatey to install SQL Server
+	$BaseCommand = "choco install $Package $VersionParam"
 
-	return $true
+	# Additional arguments for installation
+	$AdditionalArgs = "-o -ia `'/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=$Instance /INSTANCENAME=$Instance /UPDATEENABLED=FALSE /SECURITYMODE=SQL /SAPWD=$Password'` -f -y"
+
+	# Construct the command
+	$Command = "$BaseCommand $AdditionalArgs"
+
+	# Execute the command
+	Invoke-Expression $Command
+	
+	try{
+		Invoke-Sqlcmd -Query "ALTER LOGIN sa ENABLE" -ServerInstance ".\$Instance"  -TrustServerCertificate
+		Invoke-Sqlcmd -Query "ALTER LOGIN sa WITH PASSWORD='$Password'" -ServerInstance ".\$Instance"  -TrustServerCertificate
+		Invoke-Sqlcmd -Query "SELECT GETDATE() AS TimeOfQuery" -ServerInstance ".\$Instance" -Username "sa" -Password "$Password"  -TrustServerCertificate
+
+		return $true
+	}
+	catch{
+		return $false
+	}
 }
+
+# Example:
+# Install-SQLServer -Edition "Express" -Version "Latest" -Instance "SQLSERVER" -Password "SA_PASSWORD"
